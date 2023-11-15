bbcea53a69d9af3fed55d6a078c4906774d41841
Haidosama2306 <tranphanquangminh2306@gmail.com>
2023-11-15 16:47:22 +0700

    done module1: search

--- wp-content/themes/jobscout/sections/banner.php
+++ wp-content/themes/jobscout/sections/banner.php
@@ -11,17 +11,27 @@ $banner_subtitle   = get_theme_mod( 'banner_subtitle', __( 'Each month, more tha
 $find_a_job_link   = get_option( 'job_manager_jobs_page_id', 0 );
         
 if( $ed_banner && has_custom_header() ){ ?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@100&display=swap" rel="stylesheet">
+
+
     <div id="banner-section" class="site-banner<?php if( has_header_video() ) echo esc_attr( ' video-banner' ); ?>">
         <div class="item">
             <?php the_custom_header_markup(); ?>
-            <div class="banner-caption">
-                <div class="container">
-                    <div class="caption-inner">
+            <div class="banner-caption ">
+                <div class="container module_body">
+                    <div class="caption-inner ">
                         <?php 
-                            if( $banner_title ) echo '<h2 class="title">' . esc_html( $banner_title ) . '</h2>';
-                            if( $banner_subtitle ) echo '<div class="description">' . wpautop( wp_kses_post( $banner_subtitle ) ) . '</div>';
+                            if( $banner_title ) echo 
+                            '<div class="module_banner_title"> 
+                                <h2 class="title ">' 
+                                    . esc_html( $banner_title ) . 
+                                '</h2>
+                            </div>';
+                            if( $banner_subtitle ) echo '<div class="description module_banner_des">' . wpautop( wp_kses_post( $banner_subtitle ) ) . '</div>';
                         ?>
-                        <div class="form-wrap">
+                        <div class="form-wrap module_banner_searchform">
                             <div class="search-filter-wrap">
                             <?php 
                                 if ( jobscout_is_wp_job_manager_activated() ) { 
