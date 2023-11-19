7df3b7376244410be58284f637d341c79c9bd6b1
thiquyen <21211tt3163@mail.tdc.edu.vn>
2023-11-20 01:18:07 +0700

    #css

--- wp-content/themes/jobscout/inc/template-functions.php
+++ wp-content/themes/jobscout/inc/template-functions.php
@@ -162,7 +162,7 @@ if (!function_exists('jobscout_content_start')):
         echo '<div id="acc-content"><!-- .site-header -->';
         $home_sections = jobscout_get_home_sections();
         if (!(is_front_page() && !is_home() && $home_sections)) { //Make necessary adjust for pg template.
-            echo is_404() ? '<div class="error-holder">' : '<div id="content" class="site-content">';
+            echo is_404() ? '<div class="error-holder">' : '<div id="content" class="site-content news-content">';
 
             if (is_archive() || is_search() || is_page_template('templates/portfolio.php')): ?>
                     <header class="page-header">
